e79f980991d62685add72defe20614fa4a93c388
Refyandra <irev@users.noreply.github.com>
2022-08-19 00:47:18 +0700

    Update mariadb-install.sh

--- mariadb-install.sh
+++ mariadb-install.sh
@@ -2,5 +2,5 @@
 sudo apt-get install apt-transport-https curl
 sudo curl -o /etc/apt/trusted.gpg.d/mariadb_release_signing_key.asc 'https://mariadb.org/mariadb_release_signing_key.asc'
 sudo sh -c "echo 'deb https://mirror.djvg.sg/mariadb/repo/10.8/ubuntu focal main' >>/etc/apt/sources.list"
-sudo apt-get update
-sudo apt-get install mariadb-server
+sudo apt-get update -y
+sudo apt-get install mariadb-server -y
